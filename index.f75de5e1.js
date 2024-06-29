69b18f1bd2a31ee616a8b53784cb10942bd6e22f
Iryna Stefaniuk <irinagoyan@gmail.com>
2024-06-29 16:31:15 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const populationArr = [
+    ...document.querySelectorAll(".population")
+];
+const total = populationArr.reduce((sum, popul)=>sum + parseInt(popul.innerText.split(",").join("")), 0);
+const totalBox = document.querySelector(".total-population");
+const average = total / 10;
+totalBox.replaceWith(total.toLocaleString("en-US"));
+document.querySelector(".average-population").replaceWith(average.toLocaleString("en-US"));
+
+//# sourceMappingURL=index.f75de5e1.js.map
